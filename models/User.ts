d0f2d601f6af6ca4a8bf5089f96bac8b8649ee87
Joel Singh <joelsingh788@gmail.com>
2023-08-17 15:16:33 -0400

    Add User model

---
+++ models/User.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface UserProperties {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  membershipStatus: boolean;
+  admin: boolean;
+}
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  membershipStatus: {
+    type: Boolean,
+    default: false,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model<UserProperties>("User", UserSchema);
+export default User;
