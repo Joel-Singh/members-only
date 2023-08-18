d8709a5d98f6629c132f5e3e910ee9bed63d4dd0
Joel Singh <joelsingh788@gmail.com>
2023-08-18 13:39:36 -0400

    Rename

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
-const router = express.Router();
+const indexRouter = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
+indexRouter.get("/", (req, res, next) => {
   res.render("index");
 });
 
-export default router;
+export default indexRouter;
--- routes/users.ts
+++ routes/users.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
-const router = express.Router();
+const usersRouter = express.Router();
 
 /* GET users listing. */
-router.get("/", (req, res, next) => {
+usersRouter.get("/", (req, res, next) => {
   res.send("respond with a resource");
 });
 
-export default router;
+export default usersRouter;
