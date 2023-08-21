96bc19f09cf82ef15c8db7ee53e931a8396d41fb
Joel Singh <joelsingh788@gmail.com>
2023-08-21 16:30:07 -0400

    Add RequestWithSignUpFormData interface

--- types/views/signUp.d.ts
+++ types/views/signUp.d.ts
@@ -6,4 +6,14 @@ interface SignUpFormData {
   admin?: "true";
 }
 
+export interface RequestWithSignUpFormData extends Request {
+  body: {
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    admin?: "true";
+  };
+}
+
 export default signUpFormData;
